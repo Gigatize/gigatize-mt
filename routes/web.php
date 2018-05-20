f0c0413fb3bad43ab4785e1e9dd41c97b22987bb
jrichmo93 <josh.richmond93@gmail.com>
2018-05-20 13:16:29 -0500

    updated tenant auth middleware

--- routes/web.php
+++ routes/web.php
@@ -12,23 +12,19 @@
 */
 
 Route::group(['middleware' => ['web']], function () {
-    Route::domain('{customer}.' . config('app.url_base'))->group(function () {
-        //ensure tenant exists or redirect to root domain
-        Route::group(['middleware' => 'tenant.exists'], function () {
+    //authenticate tenant based on auth type
+    Route::group(['middleware' => 'tenant.login'], function () {
+        Route::domain('{customer}.' . config('app.url_base'))->group(function () {
             Route::get('verify-user/{code}', 'Auth\RegisterController@activateUser')->name('activate.user');
 
-            //authenticate tenant based on auth type
-            Route::group(['middleware' => 'tenant.login'], function () {
                 Route::get('/', function () {
                     return view('tenant');
                 });
-            });
-            Route::group(['middleware' => 'auth-type:password'], function () {
-                Route::group(['middleware' => 'tenancy.enforce'], function () {
-                    Auth::routes();
-                    Route::get('register/verify/resend', 'Auth\RegisterController@showResendVerificationEmailForm')->name('showResendVerificationEmailForm');
-                    Route::post('register/verify/resend', 'Auth\RegisterController@resendVerificationEmail')->name('resendVerificationEmail')->middleware('throttle:2,1');
-                });
+
+            Route::group(['middleware' => 'tenancy.enforce'], function () {
+                Auth::routes();
+                Route::get('register/verify/resend', 'Auth\RegisterController@showResendVerificationEmailForm')->name('showResendVerificationEmailForm');
+                Route::post('register/verify/resend', 'Auth\RegisterController@resendVerificationEmail')->name('resendVerificationEmail')->middleware('throttle:2,1');
             });
         });
     });
