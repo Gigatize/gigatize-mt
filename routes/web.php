92720d15abc550839dcec86dfd85d4582f244938
jrichmo93 <josh.richmond93@gmail.com>
2018-05-20 13:18:41 -0500

    updated tenant auth middleware

--- routes/web.php
+++ routes/web.php
@@ -14,12 +14,10 @@
 Route::group(['middleware' => ['web']], function () {
     //authenticate tenant based on auth type
     Route::group(['middleware' => 'tenant.login'], function () {
-        
-        Route::get('/', function () {
-            return view('tenant');
-        });
-
         Route::domain('{customer}.' . config('app.url_base'))->group(function () {
+            Route::get('/', function () {
+                return view('tenant');
+            });
             Route::get('verify-user/{code}', 'Auth\RegisterController@activateUser')->name('activate.user');
 
             Route::group(['middleware' => 'tenancy.enforce'], function () {
