3e3991d2a5566e668448f3dedd49fb2db338f579
jrichmo93 <josh.richmond93@gmail.com>
2018-05-20 12:51:00 -0500

    updated tenant auth middleware

--- app/Listeners/LogUserIn.php
+++ app/Listeners/LogUserIn.php
@@ -29,9 +29,6 @@ class LogUserIn
      */
     public function handle(Saml2LoginEvent $event)
     {
-        $tenancy = app(Environment::class);
-        $hostname = $tenancy->hostname();
-        dd($hostname);
         $messageId = $event->getSaml2Auth()->getLastMessageId();
         // your own code preventing reuse of a $messageId to stop replay attacks
         $user = $event->getSaml2User();
@@ -45,6 +42,8 @@ class LogUserIn
         $laravelUser = User::firstOrCreate(['email' => $user->getUserId()],['email' => $user->getUserId(), 'first_name' => $user->getAttributes()['first_name'][0],'last_name' => $user->getAttributes()['last_name'][0],'role' => $user->getAttributes()['job_title'][0], 'status' => 1]);
         //if it does not exist create it and go on or show an error message
         Auth::login($laravelUser);
+
+        dd(session()->all());
 
     }
 }
