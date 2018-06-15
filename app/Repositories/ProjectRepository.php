9e82f61b53a8c739c265ae58c4af4ad73ac5efbf
jrichmo93 <josh.richmond93@gmail.com>
2018-06-14 19:07:42 -0500

    updated project fields

--- app/Repositories/ProjectRepository.php
+++ app/Repositories/ProjectRepository.php
@@ -25,28 +25,15 @@ class ProjectRepository extends Repository
         $project->description = $data['description'];
         $project->start_date = Carbon::parse($data['start_date']);
         $project->deadline = Carbon::parse($data['deadline']);
-        $project->location_id = $data['location_id'];
-        $project->timezone = isset($data['timezone']) ?: $data['timezone'] = null;
         $project->impact = $data['impact'];
+        $project->open_to = $data['open_to'];
         $project->max_users = $data['max_users'];
         $project->estimated_hours = $data['estimated_hours'];
         $project->resources_link = isset($data['resources_link']) ?: $data['resources_link'] = null;
         $project->additional_info = isset($data['additional_info']) ?: $data['additional_info'] = null;
-        if(isset($data['flexible_start'])) {
-            $project->flexible_start = true;
-        }else{
-            $project->flexible_start = false;
-        }
-        if(isset($data['on_site'])) {
-            $project->on_site = true;
-        }else{
-            $project->on_site = false;
-        }
-        if(isset($data['renew'])) {
-            $project->renew = true;
-        }else{
-            $project->renew = false;
-        }
+        $project->flexible_start = $data['flexible_start'];
+        $project->on_site = $data['on_site'];
+        $project->renew = $data['renew'];
         $project->save();
 
         foreach ($data['acceptance_criteria'] as $criteria){
@@ -55,7 +42,7 @@ class ProjectRepository extends Repository
             ]);
         }
 
-        foreach (explode(',',$data['skills']) as $skillName){
+        foreach ($data['skills'] as $skillName){
             $skill = Skill::where('name',$skillName)->first();
             if($skill){
                 $project->Skills()->attach($skill->id);
